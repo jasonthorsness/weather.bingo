00d51948a116c91b201a8e9faa1ebf4158fa8a63
Jason Thorsness <thorsness@gmail.com>
2024-08-08 20:39:50 -0700

    fix tailwind build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "tailwindcss";
 const config: Config = {
-  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   plugins: [require("@tailwindcss/forms")],
 };
 export default config;
